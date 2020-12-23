6b2f4545bb27153fe79fe026130d76a257bab16e
dongkun <1060852789@qq.com>
2020-12-23 22:00:00 +0800

    这是项目初始化

---
+++ src/main/java/com/yunye/software/buildparent/BuildParentApplication.java
@@ -0,0 +1,13 @@
+package com.yunye.software.buildparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuildParentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yunye/software/buildparent/BuildParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunye.software.buildparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
